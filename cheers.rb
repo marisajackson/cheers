760e35f28796d234dcc25f00124f1c709d43bbc4
MaRisa Jackson <mhljackson@gmail.com>
2014-07-07 15:05:56 -0500

    part 2

--- cheers.rb
+++ cheers.rb
@@ -7,6 +7,8 @@
 #
 # Note: the “a” vs. “an”
 
+require 'date'
+
 puts "What's your name?"
 name = gets.chomp.upcase
 letters = ["A", "E", "H", "I", "L", "M", "N", "O", "R", "S", "X"]
@@ -25,4 +27,25 @@ else
     end
   end
   puts "#{name}'s just GRAND"
+
+
+
+
+  puts "Hey, #{name}, when's your birthday?"
+  birthday = gets
+  birthday = Date.parse(birthday)
+  today = Date.today
+  today = today.yday
+  birthday = birthday.yday
+  # puts Date.today - Date.parse(birthday)
+  days = today - birthday
+
+  if today > birthday
+    puts "Your birthday was #{days} days ago. Happy Late Birthday!"
+  elsif today === birthday
+    puts "Happy Birthday!"
+  else
+    days *= -1
+    puts "Your birthday is in #{days} days. Happy Early Birthday!"
+  end
 end
